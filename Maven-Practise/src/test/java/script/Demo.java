dbd9573454442e6d6c46e6ba2da87c8e6f0b6e30
TYSS <TYSS@TYSS-4F-070>
2018-04-27 11:34:14 +0530

    Commit 2

---
+++ Maven-Practise/src/test/java/script/Demo.java
@@ -0,0 +1,14 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void demoTest()
+	{
+		Reporter.log("Just Demo",true);
+	}
+
+}
