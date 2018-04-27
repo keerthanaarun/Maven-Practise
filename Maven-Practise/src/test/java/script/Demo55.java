df075215731e9b10847b84911fc6e35f90f7c041
TYSS <TYSS@TYSS-4F-087>
2018-04-27 15:55:48 +0530

    commit new55

---
+++ Maven-Practise/src/test/java/script/Demo55.java
@@ -0,0 +1,13 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo55 {
+	@Test
+	public void pushTest()
+	{
+		Reporter.log("To check push",true);
+	}
+
+}
